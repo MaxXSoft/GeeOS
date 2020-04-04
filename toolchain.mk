eb1b8d6a0f2c252daaf6c1dd1964364e24ed8d58
MaxXSoft <x@MaxXSoft.net>
2020-04-04 18:10:35 +0800

    (toolchain) update toolchain configurations

--- toolchain.mk
+++ toolchain.mk
@@ -14,26 +14,38 @@ else
 endif
 
 # cross compile toolchain prefix
-LLVM_BIN := /usr/local/opt/llvm/bin/
-YU_BIN := /Users/maxxing/Programming/MyRepo/YuLang/build/
+LLVM_BIN := /usr/local/opt/llvm/bin
+YU_BIN := /Users/maxxing/Programming/MyRepo/YuLang/build
 
 # Yu compiler
 YUFLAGS := -Werror $(YU_OPT_ARG)
 YUFLAGS := -tt riscv32-unknown-elf -tc generic-rv32 -tf +m,+a
-YUC := $(YU_BIN)yuc $(YUFLAGS)
+YUC := $(YU_BIN)/yuc $(YUFLAGS)
 
 # C compiler
 CFLAGS := -Wall -Werror -c -static $(C_DEBUG_ARG) $(C_OPT_ARG)
 # TODO: there is no '-mstrict-align' flag in clang
 CFLAGS += -fno-builtin -fno-pic
 CFLAGS += -target riscv32-unknown-elf -march=rv32ima -mabi=ilp32
-CC := $(LLVM_BIN)clang $(CFLAGS)
+CC := $(LLVM_BIN)/clang $(CFLAGS)
 
 # linker
 # LDFLAGS := -nostartfiles -nostdlib -nostdinc -melf32lriscv
 LDFLAGS :=
-LD := $(LLVM_BIN)ld.lld $(LDFLAGS)
+LD := $(LLVM_BIN)/ld.lld $(LDFLAGS)
+
+# objcopy
+OBJCFLAGS := -O binary
+OBJC := $(LLVM_BIN)/llvm-objcopy $(OBJCFLAGS)
 
 # objdump
 OBJDFLAGS := -D
-OBJD := $(LLVM_BIN)llvm-objdump $(OBJDFLAGS)
+OBJD := $(LLVM_BIN)/llvm-objdump $(OBJDFLAGS)
+
+# archiver
+ARFLAGS := ru
+AR := $(LLVM_BIN)/llvm-ar $(ARFLAGS)
+
+# ranlib
+RANLIBFLAGS :=
+RANLIB := $(LLVM_BIN)/llvm-ranlib $(RANLIBFLAGS)
