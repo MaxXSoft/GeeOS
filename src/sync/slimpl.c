a5b4530311426868d594a5937a8d0dc54f044165
MaxXSoft <x@MaxXSoft.net>
2020-04-06 17:38:59 +0800

    (spinlock) added implementation of spinlock

---
+++ src/sync/slimpl.c
@@ -0,0 +1,33 @@
+// structure definition of spinlock
+typedef struct {
+  unsigned locked;
+  const char *name;
+} Spinlock;
+
+void __acquire(Spinlock *lock) {
+  // On RISC-V, sync_lock_test_and_set turns into an atomic swap:
+  //   a5 = 1
+  //   s1 = &lock->locked
+  //   amoswap.w.aq a5, a5, (s1)
+  while(__sync_lock_test_and_set(&lock->locked, 1) != 0);
+  // Tell the C compiler and the processor to not move loads or stores
+  // past this point, to ensure that the critical section's memory
+  // references happen after the lock is acquired.
+  __sync_synchronize();
+}
+
+void __release(Spinlock *lock) {
+  // Tell the C compiler and the CPU to not move loads or stores
+  // past this point, to ensure that all the stores in the critical
+  // section are visible to other CPUs before the lock is released.
+  // On RISC-V, this turns into a fence instruction.
+  __sync_synchronize();
+  // Release the lock, equivalent to lock->locked = 0.
+  // This code doesn't use a C assignment, since the C standard
+  // implies that an assignment might be implemented with
+  // multiple store instructions.
+  // On RISC-V, sync_lock_release turns into an atomic swap:
+  //   s1 = &lock->locked
+  //   amoswap.w zero, zero, (s1)
+  __sync_lock_release(&lock->locked);
+}
