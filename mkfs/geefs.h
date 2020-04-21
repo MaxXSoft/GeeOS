fb845970d2f7ab7055228469474426b20cf98143
MaxXSoft <x@MaxXSoft.net>
2020-04-21 17:27:13 +0800

    (geefs) modified some details

--- mkfs/geefs.h
+++ mkfs/geefs.h
@@ -15,18 +15,28 @@ class GeeFS {
   GeeFS(Device &dev) : dev_(dev) {}
   ~GeeFS() { Sync(); }
 
+  // create an empty GeeFS image on device
   bool Create(std::uint32_t block_size, std::uint32_t free_map_num,
               std::uint32_t inode_blk_num);
+  // open GeeFS image on device
   bool Open();
+  // sync all modifications to device
   bool Sync();
 
+  // list all files/dirs in cwd
   void List(std::ostream &os);
+  // create new file in cwd
   bool Create(std::string_view file_name);
+  // create new directory in cwd
   bool MakeDir(std::string_view dir_name);
+  // change cwd
   bool ChangeDir(std::string_view dir_name);
+  // remove file in cwd
   bool Remove(std::string_view file_name);
+  // read file in cwd to output stream
   std::int32_t Read(std::string_view file_name, std::ostream &os,
                     std::size_t offset, std::size_t len);
+  // write input stream to file in cwd
   std::int32_t Write(std::string_view file_name, std::istream &is,
                      std::size_t offset, std::size_t len);
 
@@ -36,9 +46,12 @@ class GeeFS {
   // allocate an inode, returns inode id
   std::optional<std::uint32_t> AllocINode();
   // initialize data block of directory
-  void InitDirBlock(std::uint32_t blk_ofs, std::uint32_t parent_inode);
+  void InitDirBlock(std::uint32_t blk_ofs, std::uint32_t cur_id,
+                    std::uint32_t parent_id);
   // update inode by id
   void UpdateINode(const INode &inode, std::uint32_t id);
+  // read inode by id
+  bool ReadINode(INode &inode, std::uint32_t id);
 
   // low-level device
   Device &dev_;
