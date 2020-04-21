29972269881b70980a14f44d2896e2b34449b920
MaxXSoft <x@MaxXSoft.net>
2020-04-21 21:23:07 +0800

    (rules) added rule for compiling cpp files

--- rules.mk
+++ rules.mk
@@ -8,11 +8,16 @@ $(OBJ_DIR)/%.c.o: $(TOP_DIR)/%.c
 	-mkdir -p $(dir $@)
 	$(CC) $(CFLAGS) -o $@ $^
 
-$(OBJ_DIR)/%.o: $(OBJ_DIR)/%.ll
-	$(info LLC $@)
-	$(LLC) $(LLCFLAGS) $^ -o $@
+$(OBJ_DIR)/%.cpp.o: $(TOP_DIR)/%.cpp
+	$(info CXX $@)
+	-mkdir -p $(dir $@)
+	$(CXX) $(CXXFLAGS) -o $@ $^
 
 $(OBJ_DIR)/%.S.o: $(TOP_DIR)/%.S
 	$(info AS  $@)
 	-mkdir -p $(dir $@)
 	$(CC) $(ASFLAGS) -o $@ $^
+
+$(OBJ_DIR)/%.o: $(OBJ_DIR)/%.ll
+	$(info LLC $@)
+	$(LLC) $(LLCFLAGS) $^ -o $@
