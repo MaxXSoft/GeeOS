54bcb7cb8250b4b500eac530bc1d177519b75bd7
MaxXSoft <x@MaxXSoft.net>
2020-05-10 10:22:19 +0800

    (utils/bin2coe) added util 'bin2coe'

---
+++ utils/bin2coe.py
@@ -0,0 +1,42 @@
+#!/usr/local/bin/python3
+
+# convert binary data file to coe file
+# designed for Fuxi SoC
+# by MaxXing
+
+import sys
+
+
+def read_bin(file_name):
+  words = []
+  with open(bin_file, 'rb') as f:
+    while True:
+      x = f.read(4)
+      if x == b'':
+        break
+      word = int.from_bytes(x, byteorder='little')
+      words.append(hex(word)[2:].zfill(8))
+  return words
+
+
+def make_coe(words):
+  coe = 'memory_initialization_radix = 16;\n'
+  coe += 'memory_initialization_vector =\n'
+  contents = ['00000000'] * 128 + words
+  coe += '\n'.join(contents) + '\n'
+  return coe
+
+
+if __name__ == '__main__':
+  if len(sys.argv) < 2:
+    print('usage: ./bin2coe.py FILE')
+    exit(1)
+  
+  bin_file = sys.argv[1]
+  coe_file = f'{bin_file}.coe'
+
+  words = read_bin(bin_file)
+  coe = make_coe(words)
+  
+  with open(coe_file, 'w') as f:
+    f.write(coe)
